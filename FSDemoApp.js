0f41f3254df8d00b14be093e15a24c2232c46166
Xiaodong Li <bryne_lxd@sina.com>
2020-03-02 00:50:03 +0800

    added style to the navigation bar

--- FSDemoApp.js
+++ FSDemoApp.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';  //refer to https://reactnavigation.org/docs/en/stack-navigator.html
 import {LoginViewConnected} from "./pages/login";
 import {RegistView} from "./pages/registration";
+import {UserHome} from "./pages/userhome";
 import {LocalTest} from "./pages/localtest";
 
 const Stack = createStackNavigator();
@@ -11,12 +12,26 @@ export default class FSDemoApp extends Component {
     render() {
         return (
             <NavigationContainer>
-                <Stack.Navigator>
+                <Stack.Navigator initialRouteName="Home"
+                screenOptions={{
+                    headerStyle: {
+                      backgroundColor: '#f4511e',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                      fontWeight: 'bold',
+                    },
+                  }}>
                     <Stack.Screen name="Home" component={LoginViewConnected} />
                     <Stack.Screen name="Details" component={RegistView} /> 
+                    <Stack.Screen name="UserHome" component={UserHome} />
                 </Stack.Navigator>
             </NavigationContainer>
         );
     }
 }
+
+
+
+
 
