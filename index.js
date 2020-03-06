5142e84d11936146e4fe17b93bb9331ce3471cf0
Xiaodong Li <bryne_lxd@sina.com>
2020-03-06 23:24:08 +0800

    restore index.js from test status

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import 'react-native-gesture-handler';
 
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
-//import App from './App';
-import App from './AppNavTest';
+import App from './App';
+//import App from './AppNavTest';
 //import {TestCustomAlert as App} from './experiments';
 
 AppRegistry.registerComponent(appName, () => App);
